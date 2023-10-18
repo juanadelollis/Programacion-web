86e0cc9de099b56b1332b1613e2bdfda023c521a
juanadelollis <juanidelollis@gmail.com>
2023-10-17 21:31:52 -0300

    correcciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,10 @@
-// const menuButton = document.getElementById("menu")
-
-// const nav = document.getElementById("nav-bar")
-
-// const closeButton = document.getElementById("close")
-
-// // cuando mi usuairo hace click sobre el menu hamb quiero q se ejecute una funcion
-
-// menuButton.addEventListener("click", function(){
-//     nav.style.display = "inline-block"
-//     closeButton.style.display = "inline-block"
-//     menuButton.style.display = none
-// })
-
-
-// closeButton.addEventListener("click", function(){
-//     nav.style.display = ""
-//     menuButton.style.display = ""
-//     closeButton.style.display = ""
-// })
-
-
-let app = Vue.createApp({ //objeto con dos propiedades data y methods
-    data: function(){ //adentro tiene variables
+let app = Vue.createApp({ 
+    data: function(){ 
         return {
-            showNav: window.innerWidth > 769 ? true: false,
-            showMenuButton: window.innerWidth > 769 ? false: true,
+            showMenuButton: true,
+            showNav: true,
+            // showNav: window.innerWidth > 769 ? true: false,
+            // showMenuButton: window.innerWidth > 769 ? false: true,
             showCloseButton: false,
         }
     },
