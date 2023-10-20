723e8b6cc3dc9d5b51df64387e86e7bfce9224ba
juanadelollis <juanidelollis@gmail.com>
2023-10-20 10:57:26 -0300

    correcciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,30 @@
 let app = Vue.createApp({ 
    data: function(){ 
          return {
-             showMenuButton: true,
-             showNav: false,
-             // showNav: window.innerWidth > 769 ? true: false,
-             // showMenuButton: window.innerWidth > 769 ? false: true,
-             showCloseButton: false,
+            showMenuButton: true,
+            //showNav: true,
+            showNav: window.innerWidth > 769 ? true: false,
+            //showMenuButton: window.innerWidth > 769 ? false: true,
+            showCloseButton: false,
          }
      },
      methods:{
          toggleNav(){
              this.showNav = !this.showNav
-             this.showMenuButton = !this.showMenuButton
-             this.showCloseButton = !this.showCloseButton
+            this.showMenuButton = !this.showMenuButton
+            this.showCloseButton = !this.showCloseButton
          }
      }
 });
+
 app.mount("#app")
 
+
 //Menu hamburguesa
-// function menuHamb(){
+//function menuMobile(){
 //     const menu = document.querySelector(`menu-icon`);
-//     const list = document.querySelector(`nav-bar`);
+//    const list = document.querySelector(`nav-bar`);
+
 // }
 
 // menu.addEventListener('click', ()=> list.classList.toggle(`nav-bar-show`))
