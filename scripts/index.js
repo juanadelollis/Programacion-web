233d2123693c04baa06ac5cc0692387511b99dea
juanadelollis <juanidelollis@gmail.com>
2023-10-20 09:25:44 -0300

    correcion nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,32 @@
 let app = Vue.createApp({ 
-    data: function(){ 
-        return {
-            showMenuButton: true,
-            showNav: true,
-            // showNav: window.innerWidth > 769 ? true: false,
-            // showMenuButton: window.innerWidth > 769 ? false: true,
-            showCloseButton: false,
-        }
-    },
-    methods:{
-        toggleNav(){
-            this.showNav = !this.showNav
-            this.showMenuButton = !this.showMenuButton
-            this.showCloseButton = !this.showCloseButton
-        }
-    }
+   data: function(){ 
+         return {
+             showMenuButton: true,
+             showNav: false,
+             // showNav: window.innerWidth > 769 ? true: false,
+             // showMenuButton: window.innerWidth > 769 ? false: true,
+             showCloseButton: false,
+         }
+     },
+     methods:{
+         toggleNav(){
+             this.showNav = !this.showNav
+             this.showMenuButton = !this.showMenuButton
+             this.showCloseButton = !this.showCloseButton
+         }
+     }
 });
 app.mount("#app")
+
+//Menu hamburguesa
+// function menuHamb(){
+//     const menu = document.querySelector(`menu-icon`);
+//     const list = document.querySelector(`nav-bar`);
+// }
+
+// menu.addEventListener('click', ()=> list.classList.toggle(`nav-bar-show`))
+    
+// document.querySelector(".menu-icon").addEventListener("click", () => {
+//     document.querySelector(".nav-bar").classList.toggle("-show");
+// });
 
